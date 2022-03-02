3ebbc9755a4a2242a5bf70963da8a543635d2f20
Darimo4ka <darima_badm@yahoo.com>
2022-03-01 21:23:22 -0500

    database connection

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
+-- will create decking company employee tracker. since this is standard layout for departments in small business inviroment(constraction company, landscaping company ext.)
 INSERT INTO department (name)
 VALUES ("engineering"),
     ("finance"),
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const app = express();
+
 // Import and require mysql2
 const mysql = require('mysql2');
 
+//  this is important for deployment on HEROKU
 const PORT = process.env.PORT || 3001;
-const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+
+//  first declare app.use(express.json()) to set the base of server:
 app.use(express.json());
+// to establish the url encoding
+app.use(express.urlencoded({ extended: false }));
+
 
 // Connect to database
 const db = mysql.createConnection(
