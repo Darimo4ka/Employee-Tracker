5649d32f80ad5a3e4373f5fea3f8ffe4461e835f
Darimo4ka <darima_badm@yahoo.com>
2022-03-04 13:43:09 -0500

    create secure password and create connetion of my db with my server js

--- db/seed.sql
+++ db/seed.sql
@@ -1,19 +1,27 @@
--- will create decking company employee tracker. since this is standard layout for departments in small business inviroment(constraction company, landscaping company ext.)
-USE employees;
 INSERT INTO department (name)
 VALUES  ("Sales"),
         ("Legal"),
         ("Finance"),
         ("Engineering");
 
+SELECT * FROM DEPARTMENT;       
+
 INSERT INTO role (title, salary, department_id)
 VALUES  ("Sales Person", 50000, 1), 
         ("lawyer", 70000, 2),
-        ("Paralegal", 45000, 2);
+        ("Paralegal", 45000, 2),
+        ("Accountant", 55000, 3),
+        ("Stractural Engineer", 75000, 4);
+
+SELECT * FROM ROLE;        
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ("John", "Doe", 1,  NULL),
         ("Jenny", "Stark", 1, 1),
         ("Clark", "Mendes", 2, NULL),
-        ("Sofia", "Red", 3, 3);
+        ("Sofia", "Red", 3, 3),
+        ("Andrew", "Green", 4 , NULL),
+        ("Jennifer", "Rodriguez", 4, 5),
+        ("Armando", "Alvares", 5, NULL);
        
+SELECT * FROM employee;       
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 // Import and require mysql2
 const mysql = require('mysql2');
 
+// Load .env variables
+require("dotenv").config()
+
 //  this is important for deployment on HEROKU
 const PORT = process.env.PORT || 3001;
 
@@ -17,14 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username
-    user: 'root',
-    // Add MySQL password
-    password: '1234',
-    database: 'employees'
-  },
-  console.log(`Connected to the employees database.`)
-);
-
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: process.env.DB_USER,
+      // MySQL password
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employees database.`),
+  );
